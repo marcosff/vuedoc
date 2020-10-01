ef5d7d2f981e6581cd01c856f90721db5b933dbb
lessenlerias <backend@swissdentalservices.com>
2020-10-01 14:32:55 +0100

    correcao do async feita.

--- src/readfiles.js
+++ src/readfiles.js
@@ -18,9 +18,7 @@ class ReadFiles {
         let tempData = {}
         fs.exists(process.argv[2], async (exists) => {
             if (exists) {
-                console.log(tempData)
                 tempData = await this.openFolder(process.argv[2])
-                console.log(6)
                 console.log(tempData)
             } else {
                 console.log("caminho não encontrado")
@@ -31,16 +29,12 @@ class ReadFiles {
         let tempData = {}
         if (this.fs.lstatSync(this.folder).isDirectory()) {
             let files = this.fs.readdirSync(this.folder) 
-            await files.forEach(async (file) => {
-                console.log(file)
+
+            for (const file of files) {
                 tempData[file.replace(/ /g, "_")] = []
-                console.log(0)
                 let teste = await this.searchBlock(file);
                 tempData[file.replace(/ /g, "_")] = teste 
-                console.log(5)
-                console.log(tempData)
-            });
-
+            }
             // ESSE RETURN NAO ESTÁ OCORRENDO DEPOIS DOS AWAITs... ELE OCORRE ANTES
             return tempData
         } else {
@@ -48,7 +42,6 @@ class ReadFiles {
         }
     }
     async searchBlock(file) {        
-        console.log(1)
         let tempData = []
         
         let i = 0
@@ -58,7 +51,6 @@ class ReadFiles {
             input: this.fs.createReadStream(this.folder + file),
             crlfDelay: Infinity
         });
-        console.log(2)
         rl.on('line', (line) => {
         //for await (const line of rl) {
             if (line.includes('/**')) {
@@ -75,14 +67,11 @@ class ReadFiles {
             if (line.includes('*/')) {
                 copy = false
                 tempData.push({'info': info, 'file': file})
-                console.log(3)
                 // this.validateBlock(info, file)
             }
         })
         await once(rl, 'close');
 
-        console.log(4)
-        console.log(tempData)
         return(tempData)
 
     };
